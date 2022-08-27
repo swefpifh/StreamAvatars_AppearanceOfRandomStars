0f86a1b31ed0c781ac37f316100a5c1ba895c14e
swefpifh <90806985+swefpifh@users.noreply.github.com>
2022-08-27 13:52:07 +0200

    Update ChanceMassStars.lua

--- ChanceMassStars.lua
+++ ChanceMassStars.lua
@@ -1,17 +1,17 @@
 return function()
-	load() -- Loads the json file into memory
+	load()
 
-	local sEventSoundActivated = data.starsEventSoundActivated -- Effet sonore de lancement d'event
-	local sEventChatTextAnnonce = data.starsEventChatTextAnnonce -- Texte d'annonce dans le chat Twitch
+	local sEventSoundActivated = data.starsEventSoundActivated
+	local sEventChatTextAnnonce = data.starsEventChatTextAnnonce
 	
 	local sBubbleActivated = data.starsBubbleActivated
 	
-	local sSpawningSoundActivated = data.starsSpawningSoundActivated -- Effet sonore de spawn d'une rune
-	local sSpawnChance = data.starsSpawningChance -- Chance d'activation
+	local sSpawningSoundActivated = data.starsSpawningSoundActivated
+	local sSpawnChance = data.starsSpawningChance
 	local sSpawningNumber = math.random(data.starsSpawningNumber.Min, data.starsSpawningNumber.Max)
 	local sSpawningInterval = math.random(data.starsSpawningInterval.Min, data.starsSpawningInterval.Max) + math.random()
 	
-	if sEventSoundActivated == true then runCommand("!play " .. data.starsEventSound, true) end -- Joue le son selectionné
+	if sEventSoundActivated == true then runCommand("!play " .. data.starsEventSound, true) end
 	
 	if sEventChatTextAnnonce == 1 then writeChat("/me " .. data.starsEventChatText)
 	elseif sEventChatTextAnnonce == 2 then writeChat("/announce " .. data.starsEventChatText)
@@ -26,11 +26,11 @@ return function()
 	
 	wait(5)
 	
-	for i=1,sSpawningNumber do -- Apparition d'un nombre aléatoire d'étoiles
+	for i=1,sSpawningNumber do
 		local sSpawningCustomNumber = math.random(1, 100)
 		
 		if sSpawningCustomNumber <= sSpawnChance then
-			if sSpawningSoundActivated == true then runCommand("!play " .. data.starsSpawningSound, true) end -- Joue le son selectionné
+			if sSpawningSoundActivated == true then runCommand("!play " .. data.starsSpawningSound, true) end
 			runCommand("!spawn game", true)
 			wait(sSpawningInterval)
 		end
